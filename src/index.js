288b8f6d029b6bc4f92e330f88d0b6358f06dc95
rpm-16 <rpm-16@mail.ru>
2022-01-19 13:15:33 +0300

    логика формы гостей кривенько пока что

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
 import './sass/main.scss';
-
 import './pug/blocks/input-dropdown/input-dropdown.js';
--- src/pug/blocks/input-dropdown/input-dropdown.js
+++ src/pug/blocks/input-dropdown/input-dropdown.js
@@ -1,14 +1,55 @@
-block1.addEventListener("focus", showDropdown);
+const buttons = document.getElementsByTagName('button');
+[...buttons].forEach((button) => addEventListener('click', handler));
+block1.addEventListener('focus', showDropdown);
+
+
+// block1.addEventListener('blur', hideDropdown);
+
+
 
 function showDropdown() {
-  dropdown.style.display = 'block';
-  block1.classList.add('br');
+  obj = document.getElementsByClassName('input-dropdown-options');
+  obj[0].classList.add('expanded');
+}
+
+function hideDropdown() {
+  obj = document.getElementsByClassName('input-dropdown-options');
+  obj[0].classList.remove('expanded');
+}
+
+
+
+function handler() {
+  action = event.target.getAttribute('data-action')
+  switch (action) {
+    case 'delGuest':
+      event.target.nextElementSibling.innerHTML = parseInt(event.target.nextElementSibling.innerHTML) - 1;
+      if (parseInt(event.target.nextElementSibling.innerHTML) <= 0) {
+        event.target.setAttribute('disabled', 'disabled');
+        event.target.nextElementSibling('button').removeAttribute('disabled');
+      } 
+    break;
+    case 'addGuest':
+      event.target.previousElementSibling.innerHTML = parseInt(event.target.previousElementSibling.innerHTML) + 1;
+      if (parseInt(event.target.previousElementSibling.innerHTML) >= 5) {
+        event.target.setAttribute('disabled', 'disabled');
+        event.target.previousElementSibling('button').removeAttribute('disabled');
+      } 
+    break;
+    case 'clear':
+      let arr = document.getElementsByClassName('input-dropdown-options__itemcount');
+      for (let item of arr) {
+        item.innerHTML = 0;
+      }
+    break;
+    case 'submit':
+      let arr_list = document.getElementsByClassName('input-dropdown-options__item');
+      for (let item of arr_list) {
+        item = item.innerText.replace( '+', '');
+        item = item.replace( '-', ':');
+        const reg = /\n+/g;
+        console.log(item.replace(reg, ''));
+      }     
+    break;
   }
-
-
-function handleChangeCount(e) {
-    const liElement = $(e.currentTarget).closest('.input-dropdown__item');
-    const liIndex = liElement.attr('data-index');
-    const countType = $(e.currentTarget).attr('data-action'); // asc | desc
-    items[liIndex].count = countType === 'asc' ? items[liIndex].count + 1 : items[liIndex].count - 1;
 }
