a642d6bb200f9cd52d19fd399c5f8167f8564974
rpm-16 <rpm-16@mail.ru>
2022-01-27 00:04:44 +0300

    промежуточный коммит поковырял диапазоны пагинацию кнопки

--- src/pug/blocks/range-slider/range-slider.js
+++ src/pug/blocks/range-slider/range-slider.js
@@ -1,5 +1,15 @@
-import 'jquery';
 import 'ion-rangeslider';
 import 'ion-rangeslider/css/ion.rangeSlider.min.css';
 
-$("#rslider").ionRangeSlider();
+$('.range-slider__input').ionRangeSlider({
+    step: 100,
+    type: "double",
+    skin: "round",
+    hide_min_max: true,
+    hide_from_to: true,
+    onChange: updateValue,
+});
+
+function updateValue(data) {
+    document.getElementById("range-note").innerHTML = `${data.from} - ${data.to}`;
+}
