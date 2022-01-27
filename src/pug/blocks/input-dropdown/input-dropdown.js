79eb3fd4731391ba410c98d5a073f75f6ef3437c
rpm-16 <rpm-16@mail.ru>
2022-01-27 22:35:49 +0300

    зменил на форм группы в форм элементах все.

--- src/pug/blocks/input-dropdown/input-dropdown.js
+++ src/pug/blocks/input-dropdown/input-dropdown.js
@@ -3,12 +3,12 @@ const buttons = document.getElementsByTagName('button');
 block1.addEventListener('focus', showDropdown);
 
 function showDropdown() {
-  obj = document.getElementsByClassName('input-dropdown-options');
+  obj = document.getElementsByClassName('input-dropdown-group__options');
   obj[0].classList.add('expanded');
 }
 
 function hideDropdown() {
-  obj = document.getElementsByClassName('input-dropdown-options');
+  obj = document.getElementsByClassName('input-dropdown-group__options');
   obj[0].classList.remove('expanded');
 }
 
@@ -30,27 +30,18 @@ function handler() {
       } 
     break;
     case 'clear':
-      let arr = document.getElementsByClassName('input-dropdown-options__itemcount');
+      let arr = document.getElementsByClassName('input-dropdown-group__options__itemcount');
       for (let item of arr) {
         item.innerHTML = 0;
       }
     break;
     case 'submit':
-      block1.value = '';
-      let arr_list = document.getElementsByClassName('input-dropdown-options__item');
+      let arr_list = document.getElementsByClassName('input-dropdown-group__options__itemcount');
       let numberGuests = 0;
       for (let item of arr_list) {
-        const reg = [0-9]; /* подсчитать количество и сколнить слово */
-        numberGuests += Number(item.innerText.replace(reg, ''));
-      }
-      alert(numberGuests);
-        block1.value = str(numberGuests) + " гостя";    
-      // for (let item of arr_list) {
-      //   item = item.innerText.replace( '+', '');
-      //   item = item.replace( '-', ':');
-      //   const reg = /\n+/g;
-      //   block1.value += item.replace(reg, '');        
-      // }     
+        numberGuests += Number(item.innerText);          
+        }
+      block1.value = numberGuests + " гостей";    
       hideDropdown();
     break;
   }
