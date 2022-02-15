4b16a09fd6f577c9ffcdcaf0e0ac490265552d28
rpm-16 <rpm-16@mail.ru>
2022-02-15 22:58:24 +0300

    Календарь думала как написать его на ванильном, но решил через библиотеку сделать

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import './sass/main.scss';
 import './pug/blocks/input-dropdown/input-dropdown_native.js';
 import './pug/blocks/range-slider/range-slider.js';
 import './pug/blocks/form-group/form-group.js';
+import './pug/blocks/calendar/calendar.js';
 
